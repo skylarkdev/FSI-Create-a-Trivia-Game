c45045cc7c0013c6b521068440cd9427ff08bf18
Clayton Burns <claytonb@cooperative-networks.com>
2022-02-02 18:56:06 -0600

    Completed Code Example

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your Code Here
+let userName = window.prompt('Please enter your name:')
+let userScore = 0
+let playAgain = true
+
+function playGame(){
+    for(let i=0; i<questions.length; i++){
+        let question = questions[i]
+        let userAnswer = window.prompt(question.text)
+        if(userAnswer.toUpperCase() === question.correctAnswer){
+            userScore = userScore + 10
+        }
+    }
+    window.alert('Your score is: '+userScore)
+}
+while (playAgain === true){
+    playGame()
+    let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.')
+    if(userChoice.toUpperCase() === 'YES'){
+            playAgain = true
+    } else {
+        playAgain = false
+        window.alert('Thanks for playing the game')
+    }
+}
